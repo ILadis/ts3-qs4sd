72ec80c3f75b0c10d2a6663ba82cd598b3fb41ee
Michael Wagner <mwagn4@gmail.com>
2024-01-21 19:40:22 +0100

    Uses new flatpak name and adds utility class to call client lib

--- main.py
+++ main.py
@@ -4,20 +4,27 @@ import tempfile
 import subprocess
 import tarfile
 import urllib
+import ctypes
 
 import decky_plugin
 
 # Note: all plugin arguments are pointing to Plugin and not to an instance of it
 class Plugin:
   async def _main(plugin):
-    plugin.teamspeak = NativeTeamSpeak()
+    plugin.teamspeak = FlatpakTeamSpeak()
+    plugin.clientlib = Clientlib()
 
   async def install(plugin):
-    decky_plugin.logger.debug('Attempt to install TeamSpeak 3')
+    decky_plugin.logger.debug('Attempting to install TeamSpeak 3')
     plugin.teamspeak.install()
 
   async def start(plugin):
-    decky_plugin.logger.debug('Attempt to starting TeamSpeak 3')
+    decky_plugin.logger.debug('Attempting to install TeamSpeak 3 client plugin')
+
+    file = plugin.clientlib.filepath
+    plugin.teamspeak.install_plugin(file)
+
+    decky_plugin.logger.debug('Attempting to start TeamSpeak 3')
     plugin.teamspeak.start()
 
   async def status(plugin):
@@ -29,23 +36,27 @@ class Plugin:
     return dict(running = running, installed = installed)
 
   async def stop(plugin):
-    decky_plugin.logger.debug('Attempt to stop TeamSpeak 3')
+    decky_plugin.logger.debug('Attempting to stop TeamSpeak 3')
     plugin.teamspeak.stop()
 
   async def _unload(plugin):
     decky_plugin.logger.debug('Unloading plugin, attempt to stop TeamSpeak 3')
     plugin.teamspeak.stop()
 
+class Clientlib:
+  def __init__(self):
+    self.filepath = decky_plugin.DECKY_PLUGIN_DIR + '/bin/ts3-qs4sd.so'
+    self.handle = ctypes.cdll.LoadLibrary(self.filepath)
+
+  def add_bookmark(self, dbpath, nickname, bookmark_name, address, port):
+    add = self.handle.TS3BookmarkManager_addBookmark
+    add.restype = ctypes.c_bool
+    add.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_uint]
+    return add(dbpath, nickname, bookmark_name, address, port)
+
 class TeamSpeak:
-  @staticmethod
-  def homedir():
-    homedir = decky_plugin.DECKY_USER_HOME
-    appsdir = '.var/app/com.teamspeak.TeamSpeak'
-
-    if os.path.exists(f'{homedir}/{appsdir}'):
-      homedir = f'{homedir}/{appsdir}'
-
-    return homedir
+  def configdir():
+    pass
 
   def is_running(self):
     return False
@@ -62,25 +73,32 @@ class TeamSpeak:
   def install(self):
     return False
 
-  def install_plugin(self, plugindir, homedir):
-    filename = 'ts3-qs4sd.so'
-    srcfile = f'{plugindir}/bin/{filename}'
-    dstfile = f'{homedir}/.ts3client/plugins/{filename}'
+  def install_plugin(self, file):
+    if not self.is_installed() or self.is_running():
+      return False
 
-    if os.path.exists(dstfile):
-      os.chmod(dstfile, 0o755)
+    filename = os.path.basename(file)
+    target = self.configdir() + '/plugins/' + filename
+
+    if os.path.exists(target):
+      os.chmod(target, 0o755)
 
     try:
-      shutil.copy(srcfile, dstfile)
+      shutil.copy(file, target)
       return True
     except:
       return False
 
 class NativeTeamSpeak(TeamSpeak):
-  process = None
-  version = '3.6.2'
-  installdir = '/tmp/teamspeak3'
-  executable = 'ts3client_linux_amd64'
+  def __init__(self):
+    self.process = None
+    self.version = '3.6.2'
+    self.installdir = '/tmp/teamspeak3'
+    self.executable = 'ts3client_linux_amd64'
+
+  def configdir(self):
+    homedir = decky_plugin.DECKY_USER_HOME
+    return f'{homedir}/.ts3client'
 
   def is_running(self):
     if self.process is None:
@@ -99,10 +117,8 @@ class NativeTeamSpeak(TeamSpeak):
     executable = f'{self.installdir}/{self.executable}'
 
     uid = os.getuid()
-    homedir = TeamSpeak.homedir()
 
     env = dict(os.environ)
-    env['HOME'] = homedir
     env['DISPLAY'] = ':0'
     env['PULSE_SERVER'] = f'unix:/run/user/{uid}/pulse/native'
     env['PULSE_CLIENTCONFIG'] = f'/run/user/{uid}/pulse/config'
@@ -153,19 +169,20 @@ class NativeTeamSpeak(TeamSpeak):
       tar.extractall(path=self.installdir)
 
       file.close()
+      return True
     except:
       return False
 
-    plugindir = decky_plugin.DECKY_PLUGIN_DIR
-    homedir = TeamSpeak.homedir()
-
-    return self.install_plugin(plugindir, homedir)
-
 class FlatpakTeamSpeak(TeamSpeak):
+  def configdir(self):
+    homedir = decky_plugin.DECKY_USER_HOME
+    appsdir = '.var/app/com.teamspeak.TeamSpeak3'
+    return f'{homedir}/{appsdir}/.ts3client'
+
   def is_running(self):
     try:
       ps = subprocess.check_output(['flatpak', 'ps'], encoding='utf-8')
-      return 'com.teamspeak.TeamSpeak' in ps
+      return 'com.teamspeak.TeamSpeak3' in ps
     except:
       return False
 
@@ -181,7 +198,7 @@ class FlatpakTeamSpeak(TeamSpeak):
     env['PULSE_CLIENTCONFIG'] = f'/run/user/{uid}/pulse/config'
 
     try:
-      subprocess.Popen(['flatpak', 'run', 'com.teamspeak.TeamSpeak'], env=env)
+      subprocess.Popen(['flatpak', 'run', 'com.teamspeak.TeamSpeak3'], env=env)
     except:
       pass
 
@@ -190,14 +207,14 @@ class FlatpakTeamSpeak(TeamSpeak):
       return
 
     try:
-      subprocess.run(['flatpak', 'kill', 'com.teamspeak.TeamSpeak'])
+      subprocess.run(['flatpak', 'kill', 'com.teamspeak.TeamSpeak3'])
     except:
       pass
 
   def is_installed(self):
     try:
       ls = subprocess.check_output(['flatpak', 'list'], encoding='utf-8')
-      return 'com.teamspeak.TeamSpeak' in ls
+      return 'com.teamspeak.TeamSpeak3' in ls
     except:
       return False
 
@@ -210,10 +227,6 @@ class FlatpakTeamSpeak(TeamSpeak):
 
     try:
       subprocess.Popen(['flatpak', 'install', 'com.teamspeak.TeamSpeak3', '--noninteractive'])
+      return True
     except:
       return False
-
-    plugindir = decky_plugin.DECKY_PLUGIN_DIR
-    homedir = TeamSpeak.homedir()
-
-    return self.install_plugin(plugindir, homedir)
